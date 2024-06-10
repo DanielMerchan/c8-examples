7020af9361a1b63f657902559f3b68e5e5e996bb
DanielMerchan <daniel.merchan.garcia@gmail.com>
2024-06-10 21:25:16 +0200

    Example working fully in Local Dev Environment

--- zeebe-java-client-hello-world/src/main/java/com/merchan/camunda/helloworld/HelloWorldApplication.java
+++ zeebe-java-client-hello-world/src/main/java/com/merchan/camunda/helloworld/HelloWorldApplication.java
@@ -1,12 +1,18 @@
 package com.merchan.camunda.helloworld;
 
+import com.merchan.camunda.helloworld.handler.SayHelloHandler;
 import com.merchan.camunda.helloworld.zeebe.ZeebeClientFactoryProvider;
 import io.camunda.zeebe.client.ZeebeClient;
 import io.camunda.zeebe.client.api.response.BrokerInfo;
 import io.camunda.zeebe.client.api.response.PartitionInfo;
+import io.camunda.zeebe.client.api.response.ProcessInstanceEvent;
+import io.camunda.zeebe.client.api.worker.JobWorker;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-import java.net.InetSocketAddress;
 import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
 
 /**
  * Main class
@@ -14,24 +20,54 @@ import java.util.List;
  */
 public final class HelloWorldApplication {
 
-    public HelloWorldApplication() {
-    }
+    private static final Logger LOG = LogManager.getLogger(HelloWorldApplication.class);
+    private static final String HELLO_WORLD_PROCESS_ID = "Process_HelloWorld";
 
     public static void main(final String[] args) {
         try (ZeebeClient zeebeClient = ZeebeClientFactoryProvider.getFactory().create()) {
-            System.out.println("Hello World!");
-            List<BrokerInfo> brokers = zeebeClient
-                    .newTopologyRequest()
+            // Debug details about the broker to ensure we are connected
+            if (LOG.isDebugEnabled()) {
+                HelloWorldApplication.printBrokerInfo(zeebeClient);
+            }
+            // 1. Deploy hello-world.bpmn
+            zeebeClient.newDeployResourceCommand()
+                    .addResourceFromClasspath("hello-world.bpmn")
                     .send()
-                    .join()
-                    .getBrokers();
+                    .join();
+            // 2. Attach Job Worker for sayHello
+            final JobWorker sayHelloWorker = zeebeClient.newWorker()
+                    .jobType(SayHelloHandler.JOB_TYPE)
+                    .handler(new SayHelloHandler())
+                    .open();
+            // 3. Create process instance of hello-world.bpmn
+            final ProcessInstanceEvent processInstanceEvent = zeebeClient.newCreateInstanceCommand()
+                    .bpmnProcessId(HelloWorldApplication.HELLO_WORLD_PROCESS_ID)
+                    .latestVersion()
+                    .variables(Map.of("name", "Daniel Goose"))
+                    .send()
+                    .join();
+            LOG.info(processInstanceEvent.toString());
 
-            for (var broker : brokers) {
-                System.out.println(broker.getAddress());
-                List<PartitionInfo> partitions = broker.getPartitions();
-                for (var partition : partitions) {
-                    System.out.println(partition.getPartitionId());
-                }
+            // 4. Keep service alive for more job workers processing (if needed)
+            Scanner sc = new Scanner(System.in);
+            sc.nextInt();
+            sc.close();
+            sayHelloWorker.close();
+        }
+    }
+
+    private static void printBrokerInfo(final ZeebeClient zeebeClient) {
+        List<BrokerInfo> brokers = zeebeClient
+                .newTopologyRequest()
+                .send()
+                .join()
+                .getBrokers();
+
+        for (var broker : brokers) {
+            LOG.debug(broker.toString());
+            List<PartitionInfo> partitions = broker.getPartitions();
+            for (var partition : partitions) {
+                LOG.debug("partition: {}", partition.toString());
             }
         }
     }
---
+++ zeebe-java-client-hello-world/src/main/java/com/merchan/camunda/helloworld/handler/SayHelloHandler.java
@@ -0,0 +1,36 @@
+package com.merchan.camunda.helloworld.handler;
+
+import io.camunda.zeebe.client.api.response.ActivatedJob;
+import io.camunda.zeebe.client.api.worker.JobClient;
+import io.camunda.zeebe.client.api.worker.JobHandler;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Say hello - job handler which will be associated to the "sayHello" job type
+ * @author dmerchang
+ */
+public final class SayHelloHandler implements JobHandler {
+
+    private static final Logger LOG = LogManager.getLogger(SayHelloHandler.class);
+
+    public static final String JOB_TYPE = "sayHello";
+
+    /**
+     * Handles the job sayHello
+     * @param client - JobClient for sending a command to Zeebe about the job result
+     * @param job - Contains the activated job information along with the variables
+     * @throws Exception
+     */
+    @Override
+    public void handle(JobClient client, ActivatedJob job) throws Exception {
+        final String name = (String) job.getVariablesAsMap().get("name");
+        LOG.info("Saying hello '{}'", name);
+        client.newCompleteCommand(job.getKey()).send()
+                .whenComplete((result, exception) -> {
+                    if (exception != null) {
+                        LOG.error("Failed to complete job {} with exception", job.getKey(), exception);
+                    }
+                });
+    }
+}
